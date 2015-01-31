449e0cb94c28a1c2826d400598f231d4948e1fcb
Zach Moore <zcmoore@asu.edu>
2015-01-31 01:12:35 -0700

    Initial commit

---
+++ desktop/src/com/lobsterkit/fragmentary/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lobsterkit.fragmentary.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lobsterkit.fragmentary.FragmentaryEdiquet;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FragmentaryEdiquet(), config);
+	}
+}
